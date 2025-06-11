3f4e6f8a720eb9bc6e1d16709d2f26ef43aa2267
Sajith-Priyankara <e19304@eng.pdn.ac.lk>
2025-06-11 11:24:08 +0530

    swith cases

--- frontend-ecom-react-app/src/redux/reducer.js
+++ frontend-ecom-react-app/src/redux/reducer.js
@@ -1,11 +1,18 @@
 import { ADD_TO_CART } from "./constant";
 
 export const cartData = (payload = [], action) => {
-  if (action.type === ADD_TO_CART) {
-    console.warn("reducer is called", action);
-    //some logic here
-    return payload;
-  } else {
-    return "no Action is called";
+  //   if (action.type === ADD_TO_CART) {
+  //     console.warn("reducer is called", action);
+  //     //some logic here
+  //     return payload;
+  //   } else {
+  //     return "no Action is called";
+  //   }
+  switch (action.type) {
+    case ADD_TO_CART:
+      console.warn("reducer is called", action);
+      return payload;
+    default:
+      return "no Action is called";
   }
 };
